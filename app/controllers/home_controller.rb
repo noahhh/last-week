4a4a38a8f093010503e2e995819cf58ed9bc522f
gregurton <gburt802@gmail.com>
2014-11-09 17:15:27 -0500

    simplified user model and controllers, adjusted erb in views accordingly to agree

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   #this needs a lot of work
   def index
-    if logged_in?
+    if current_user
       lastfm_auth = current_user.users.find_by(provider: :lastfm)
       if lastfm_auth
         response = HTTParty.get("http://ws.audioscrobbler.com/2.0/",
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/auth/:provider/callback', to: "sessions#create"
+  get 'signout', to: 'sessions#destroy', as: 'signout'
   root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
